8e8426f5aa5d8a8be7863911549acd701cd1d895
daegyu-song <141833286+daegyu-song@users.noreply.github.com>
2026-05-18 17:56:10 +0900

    init: add initial project structure with Gradle and Spring Boot

---
+++ src/main/java/com/ssafy/culturepick/CulturepickApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.culturepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturepickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulturepickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/culturepick/CulturepickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.culturepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturepickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
