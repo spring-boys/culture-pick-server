91364c10ce37b9cebe5a04c32803d70859d1824a
sdg <sdk0109@naver.com>
2026-06-25 11:33:45 +0900

    fix: OAuth2 redirect URI 설정 및 prod ddl-auto none으로 변경

--- src/main/java/com/ssafy/culturepick/culture/controller/CultureDevController.java
+++ src/main/java/com/ssafy/culturepick/culture/controller/CultureDevController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile({"local", "prod"})
+@Profile("local")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/dev")
